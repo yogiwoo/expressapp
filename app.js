af84eb7d96dced92d4c41e3ade1e1194f56f7a7d
yogesh kumar <yogiwoo@gmail.com>
2025-12-26 23:34:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('all correct');
+});
+
+app.get('/health', (req, res) => {
+    res.send('FINE');
+});
+
+app.listen(PORT, () => {
+    console.log('app started', PORT);
+});
